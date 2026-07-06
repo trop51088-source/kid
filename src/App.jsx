c4ed62d6588142d2fe3a00ba8d36d5383d6bb77c
trop51088-source <trop51088@gmail.com>
2026-07-06 21:27:01 +0300

    iOS-style liquid glass tab bar

--- src/App.jsx
+++ src/App.jsx
@@ -1360,7 +1360,9 @@ const App = () => {
         </button>
         <button className="nav-btn" style={getNavBtnStyle(1)} onClick={() => setActiveTab('home')}>
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" width="22" height="22">
-            <path d="M3 9.5L12 3l9 6.5V20a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9.5z" /><polyline points="9 21 9 12 15 12 15 21" />
+            <path d="M5 12L3 12L12 3L21 12L19 12" />
+            <path d="M5 12V19a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V12" />
+            <path d="M9 21V15a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2V21" />
           </svg>
           <span>Главная</span>
         </button>
